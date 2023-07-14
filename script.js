e07d3c66d4082d9fe6b4f950763cee024724a297
HilalBodon <hilal_bodon@hotmail.com>
2023-07-14 06:59:38 +0300

    adding names to score

--- script.js
+++ script.js
@@ -1,63 +1,65 @@
+
 let cells = ['', '', '', '', '', '', '', '', ''];
 let currentPlayer = 'X';
 
 let result = document.querySelector('.result');
+
 let resultX = document.querySelector('.resultX');
 let resultY = document.querySelector('.resultY');
-let scoreValueX= document.querySelector('.scoreValueX');
-let scoreValueY= document.querySelector('.scoreValueY');
 
+let scoreValueX = document.querySelector('.scoreValueX');
+let scoreValueY = document.querySelector('.scoreValueY');
+
+let playerXname = document.querySelector('#playerXname');
+let playerOname = document.querySelector('#playerOname');
 
 let btns = document.querySelectorAll('.btn');
 let conditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
 ];
 
-
-function score(winner){
-    if (winner== "X"){
-        scoreValueX.innerHTML ++;
-        console.log(scoreValueX.innerHTML,scoreValueY.innerHTML);
-    }else{
-        scoreValueY.innerHTML ++;
-        console.log(scoreValueX.innerHTML,scoreValueY.innerHTML);
-
-    }
+function score(winner) {
+  if (winner == 'X') {
+    scoreValueX.innerHTML++;
+  } else {
+    scoreValueY.innerHTML++;
+  }
 }
 
 const ticTacToe = (element, index) => {
-    element.value = currentPlayer;
-    element.disabled = true;
-    cells[index] = currentPlayer;
-    currentPlayer = currentPlayer == 'X' ? 'O' : 'X'; //condition
-    result.innerHTML = `Player ${currentPlayer} Turn`;
+  element.value = currentPlayer;
+  element.disabled = true;
+  cells[index] = currentPlayer;
+  currentPlayer = currentPlayer == 'X' ? 'O' : 'X';
+  result.innerHTML = `Player ${currentPlayer} Turn`;
 
-    for (let i = 0; i < conditions.length; i++) {
-        let condition = conditions[i];
-        let a = cells[condition[0]];
-        let b = cells[condition[1]];
-        let c = cells[condition[2]];
+  for (let i = 0; i < conditions.length; i++) {
+    let condition = conditions[i];
+    let a = cells[condition[0]];
+    let b = cells[condition[1]];
+    let c = cells[condition[2]];
 
-        if (a == '' || b == '' || c == '') {
-            continue;
-        }
-
-        if ((a == b) && (b == c)) {
-            result.innerHTML = `Player ${a} Won`;
-            btns.forEach((btn) => btn.disabled = true);
-            score(a);
-
-        }
-            
+    if (a == '' || b == '' || c == '') {
+      continue;
     }
 
+    if (a == b && b == c) {
+      if (a == 'X') {
+        result.innerHTML = `Player ${playerXname.value} Won`;
+      } else {
+        result.innerHTML = `Player ${playerOname.value} Won`;
+      }
+      btns.forEach((btn) => (btn.disabled = true));
+      score(a);
+    }
+  }
 };
 
 
